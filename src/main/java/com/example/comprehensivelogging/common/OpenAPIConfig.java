ad89db40d01f82ee9df84950b8b07132147dbb72
Sielei Herman <hsielei@gmail.com>
2024-06-06 13:29:15 +0300

    configure springdoc OpenAPI

---
+++ src/main/java/com/example/comprehensivelogging/common/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package com.example.comprehensivelogging.common;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Comprehensive Logging")
+                        .description("A simple REST API project that is used to demonstrate logging " +
+                                "capabilities of Spring Boot.")
+                        .version("0.0.1-SNAPSHOT"));
+    }
+}
