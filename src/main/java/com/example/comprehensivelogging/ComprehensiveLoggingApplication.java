a423b5e7178693c6caaceb61139ddb57e6d1bf65
Sielei Herman <hsielei@gmail.com>
2024-06-06 11:39:05 +0300

    set up spring boot project

---
+++ src/main/java/com/example/comprehensivelogging/ComprehensiveLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.comprehensivelogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprehensiveLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComprehensiveLoggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comprehensivelogging/ComprehensiveLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comprehensivelogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprehensiveLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
